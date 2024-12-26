03d4e158ad604ba2c706ca252221cb1a3c615872
Alex Perkins <alex.perkins@protonmail.com>
2024-12-26 13:43:12 -0500

    updated to use a netconf filter

--- netconf-1.py
+++ netconf-1.py
@@ -1,4 +1,5 @@
 from ncclient import manager
+import xml.dom.minidom
 
 router = {
 	"host": "devnetsandboxiosxe.cisco.com",
@@ -7,6 +8,21 @@ router = {
 	"password": "C1sco12345"
 }
 
+netconf_filter = """
+<filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+	<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+		<interface>
+			<name>GigabitEthernet3</name>
+		</interface>
+	</interfaces>
+	<interfaces-state xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+		<interface>
+			<name>GigabitEthernet3</name>
+		</interface>
+	</interfaces-state>
+</filter>
+"""
+
 with manager.connect(
 	host=router["host"],
 	port=router["port"],
@@ -14,5 +30,9 @@ with manager.connect(
 	password=router["password"],
 	hostkey_verify=False
 ) as m:
-	for capability in m.server_capabilities:
-		print(capability)
+	# for capability in m.server_capabilities:
+	# 	print(capability)
+	interface_netconf = m.get(netconf_filter)
+	xmlDom = xml.dom.minidom.parseString(str(interface_netconf))
+	print(xmlDom.toprettyxml(indent="  "))
+	print('*' * 25 + 'Break' + '*' * 50)
